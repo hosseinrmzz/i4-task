022162d5ae314b14d0854273a9e286bdec77dcf8
Hossein Rmz <hosseinrmz@Hosseins-MacBook-Air.local>
2023-06-25 20:17:37 +0330

    fixed bugs

--- src/app/main/data.service.ts
+++ src/app/main/data.service.ts
@@ -11,7 +11,6 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   api: string = 'https://gist.githubusercontent.com/hosseinrmzz/efc2853bf4aa386266a4cce7fce572dd/raw/906e9b47e80f2c1746c161b2631e86bda5afb814/data.json'
-  data!: any[]
 
   request(): Observable<any> {
     return this.http.get(this.api);
